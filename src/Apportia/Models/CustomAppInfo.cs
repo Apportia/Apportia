6f433c5be408736943de32ca7eb9e1798e6078f1
koryboc <admin@si13n7.com>
2026-08-03 00:06:02 +0200

    refactor: omit default and empty fields when writing custom app database

--- src/Apportia/Models/CustomAppInfo.cs
+++ src/Apportia/Models/CustomAppInfo.cs
@@ -1,7 +1,9 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Apportia.Models;
 
+[JsonConverter(typeof(CustomAppInfoConverter))]
 public sealed class CustomAppInfo
 {
     public string Name { get; set; } = string.Empty;
@@ -20,6 +22,105 @@ public sealed class CustomAppInfo
     public bool UpdateEnabled { get; set; } = true;
     public string[] LeftoverKnown { get; set; } = [];
     public string[] LeftoverDelete { get; set; } = [];
+}
+
+public sealed class CustomAppInfoConverter : JsonConverter<CustomAppInfo>
+{
+    public override CustomAppInfo Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+            throw new JsonException();
+        var info = new CustomAppInfo();
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                return info;
+            if (reader.TokenType != JsonTokenType.PropertyName)
+                throw new JsonException();
+            var name = reader.GetString()!;
+            reader.Read();
+            switch (name)
+            {
+                case nameof(CustomAppInfo.Name): info.Name = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.Description): info.Description = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.ExeFile): info.ExeFile = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.Website): info.Website = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.Category): info.Category = reader.GetString() ?? "Advanced"; break;
+                case nameof(CustomAppInfo.SubCategory): info.SubCategory = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.JoinedDate): info.JoinedDate = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.DisplayVersion): info.DisplayVersion = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.PackageVersion): info.PackageVersion = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.VersionSource): info.VersionSource = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.UpdateDate): info.UpdateDate = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.DownloadPath): info.DownloadPath = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.DownloadFile): info.DownloadFile = reader.GetString() ?? ""; break;
+                case nameof(CustomAppInfo.UpdateEnabled): info.UpdateEnabled = reader.GetBoolean(); break;
+                case nameof(CustomAppInfo.LeftoverKnown): info.LeftoverKnown = ReadStringArray(ref reader); break;
+                case nameof(CustomAppInfo.LeftoverDelete): info.LeftoverDelete = ReadStringArray(ref reader); break;
+                default: reader.Skip(); break;
+            }
+        }
+
+        throw new JsonException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, CustomAppInfo value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        WriteString(writer, nameof(value.Name), value.Name);
+        WriteString(writer, nameof(value.Description), value.Description);
+        WriteString(writer, nameof(value.ExeFile), value.ExeFile);
+        WriteString(writer, nameof(value.Website), value.Website);
+        if (value.Category != "Advanced")
+            WriteString(writer, nameof(value.Category), value.Category);
+        WriteString(writer, nameof(value.SubCategory), value.SubCategory);
+        WriteString(writer, nameof(value.JoinedDate), value.JoinedDate);
+        WriteString(writer, nameof(value.DisplayVersion), value.DisplayVersion);
+        WriteString(writer, nameof(value.PackageVersion), value.PackageVersion);
+        WriteString(writer, nameof(value.VersionSource), value.VersionSource);
+        WriteString(writer, nameof(value.UpdateDate), value.UpdateDate);
+        WriteString(writer, nameof(value.DownloadPath), value.DownloadPath);
+        WriteString(writer, nameof(value.DownloadFile), value.DownloadFile);
+        if (!value.UpdateEnabled)
+            writer.WriteBoolean(nameof(value.UpdateEnabled), false);
+        WriteArray(writer, nameof(value.LeftoverKnown), value.LeftoverKnown);
+        WriteArray(writer, nameof(value.LeftoverDelete), value.LeftoverDelete);
+        writer.WriteEndObject();
+    }
+
+    private static void WriteString(Utf8JsonWriter writer, string name, string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+            writer.WriteString(name, value);
+    }
+
+    private static void WriteArray(Utf8JsonWriter writer, string name, string[] values)
+    {
+        if (values.Length == 0)
+            return;
+        writer.WritePropertyName(name);
+        writer.WriteStartArray();
+        foreach (var v in values)
+            writer.WriteStringValue(v);
+        writer.WriteEndArray();
+    }
+
+    private static string[] ReadStringArray(ref Utf8JsonReader reader)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException();
+        var list = new List<string>();
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndArray)
+                return list.ToArray();
+            if (reader.TokenType != JsonTokenType.String)
+                throw new JsonException();
+            list.Add(reader.GetString()!);
+        }
+
+        throw new JsonException();
+    }
 }
 
 [JsonSerializable(typeof(CustomAppInfo))]
